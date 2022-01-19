6dfc743e4e1ff259e08475520d0ed04d77752b4d
FabijanC <fabijan.corak@gmail.com>
2022-01-19 19:59:08 +0100

    Remove poetry show before

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -8,8 +8,6 @@ echo "python3 path:"
 which python3
 
 pip3 install poetry
-echo "poetry show before:"
-poetry show -v
 poetry config virtualenvs.create false
 echo "poetry show after:"
 poetry show -v
