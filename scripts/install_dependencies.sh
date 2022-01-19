2bc5f1e30240fdb8beb03d96234ab74eb7e38c83
FabijanC <fabijan.corak@gmail.com>
2022-01-19 19:33:42 +0100

    Log path

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,9 +2,14 @@
 
 set -e
 
+echo "Path before install:"
+echo "$PATH"
+
 pip3 install poetry
 poetry config virtualenvs.create false
 poetry install
 
+echo "Path after install:"
+echo "$PATH"
 echo "Starknet version:"
 starknet --version 
