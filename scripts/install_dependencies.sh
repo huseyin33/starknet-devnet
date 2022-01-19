f6b087726ec11634fe37733c03e4171bc1910f85
FabijanC <fabijan.corak@gmail.com>
2022-01-19 19:56:14 +0100

    Log python+poetry path

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,16 +2,19 @@
 
 set -e
 
-echo "Path before install:"
-echo "$PATH"
+echo "python path:"
+which python
+echo "python3 path:"
+which python3
 
 pip3 install poetry
+echo "poetry show before:"
+poetry show -v
 poetry config virtualenvs.create false
+echo "poetry show after:"
+poetry show -v
+
 poetry install
 
-echo "Path after install:"
-echo "$PATH"
-echo "Flask version:"
-flask --version
 echo "Starknet version:"
 starknet --version 
