e07b6bfa46ad1774afaeb1ca835e6653c055f741
FabijanC <fabijan.corak@gmail.com>
2022-02-16 12:08:36 +0100

    Add monkey-patch for parsing

--- starknet_devnet/contract_wrapper.py
+++ starknet_devnet/contract_wrapper.py
@@ -3,10 +3,18 @@ Contains code for wrapping StarknetContract instances.
 """
 
 from dataclasses import dataclass
-from typing import List
+from typing import List, Tuple
 
+from starkware.cairo.lang.compiler.ast.cairo_types import (
+    CairoType,
+    TypeFelt,
+    TypePointer
+)
+from starkware.cairo.lang.compiler.parser import parse_type
+from starkware.cairo.lang.compiler.type_system import mark_type_resolved
 from starkware.starknet.public.abi import get_selector_from_name
 from starkware.starknet.services.api.contract_definition import ContractDefinition
+from starkware.starknet.testing import contract
 from starkware.starknet.testing.contract import StarknetContract
 from starkware.starknet.testing.objects import StarknetTransactionExecutionInfo
 
@@ -22,11 +30,51 @@ def extract_types(abi):
     type_dict = { struct["name"]: struct for struct in structs }
     return type_dict
 
+def patched_parse_arguments(arguments_abi: dict) -> Tuple[List[str], List[CairoType]]:
+    """
+    Given the input or output field of a StarkNet contract function ABI,
+    computes the arguments that the python proxy function should accept.
+    In particular, an array input that has two inputs in the
+    original ABI (foo_len: felt, foo: felt*) will be converted to a single argument foo.
+
+    Returns the argument names and their Cairo types in two separate lists.
+    """
+    print("DEBUG received arguments_abi", arguments_abi)
+    arg_names: List[str] = []
+    arg_types: List[CairoType] = []
+    for arg_entry in arguments_abi:
+        name = arg_entry["name"]
+        arg_type = mark_type_resolved(parse_type(code=arg_entry["type"]))
+        if isinstance(arg_type, TypePointer):
+            size_arg_actual_name = arg_names.pop()
+            actual_type = arg_types.pop()
+            # Make sure the last argument was {name}_len, and remove it.
+            len_arg_name = f"{name}_len"
+            patched_size_arg_name = f"{name}_size"
+            assert (
+                size_arg_actual_name in [len_arg_name, patched_size_arg_name]
+            ), f"Array size argument {len_arg_name} must appear right before {name}."
+
+            assert isinstance(actual_type, TypeFelt), (
+                f"Array size entry {len_arg_name} expected to be type felt. Got: "
+                f"{actual_type.format()}."
+            )
+
+        arg_names.append(name)
+        arg_types.append(arg_type)
+
+    print("DEBUG returning arg_names", arg_names)
+    print("DEBUG returning arg_types", arg_types)
+    return arg_names, arg_types
+
+contract.parse_arguments = patched_parse_arguments
+
 @dataclass
 class ContractWrapper:
     """
     Wraps a StarknetContract, storing its types and code for later use.
     """
+    # pylint: disable=redefined-outer-name
     def __init__(self, contract: StarknetContract, contract_definition: ContractDefinition):
         self.contract: StarknetContract = contract
         self.code: dict = {
