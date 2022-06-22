5681acc517c0033574370e25299ae322467a6952
Toni Tabak <tabaktoni@gmail.com>
2022-06-22 20:27:35 +0200

    #2fyzj2e ✅ Test predeployed fixed values, 💄Cleanup

--- test/support/assertions.py
+++ test/support/assertions.py
@@ -4,18 +4,18 @@ from jsonschema import validate, RefResolver
 
 
 def assert_valid_schema(data, schema_file):
-    """ Checks whether the given data matches the schema """
+    """Checks whether the given data matches the schema"""
 
     schema = _load_json_schema(schema_file)
-    schema_dir = join(dirname(__file__), 'schemas')
-    resolver = RefResolver(base_uri = 'file://' + schema_dir + '/', referrer = schema)
-    return validate(data, schema, resolver = resolver)
+    schema_dir = join(dirname(__file__), "schemas")
+    resolver = RefResolver(base_uri="file://" + schema_dir + "/", referrer=schema)
+    return validate(data, schema, resolver=resolver)
 
 
 def _load_json_schema(filename):
-    """ Loads the given schema file """
+    """Loads the given schema file"""
 
-    relative_path = join('schemas', filename)
+    relative_path = join("schemas", filename)
     absolute_path = join(dirname(__file__), relative_path)
 
     with open(absolute_path) as schema_file:
